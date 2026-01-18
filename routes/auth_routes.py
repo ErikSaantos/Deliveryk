aa725e15a03253692311128c0d420ab8bc59075c
ErikSantos <erik.silva@picpay.com>
2026-01-17 22:26:46 -0300

    Add auth_routes and order_routes

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -4,7 +4,7 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get('/')
 async def autenticar():
-    """"
+    """
     Essa é a rota padrão de autenticação do nosso sistema.
     """
     return {"msg": "Acessou a rota de autenticação", "autenticado": True}
